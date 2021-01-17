90fa7d3f7522153b2d12af83ef58967b200b73e6
Nikita <nikita199801@gmail.com>
2021-01-17 18:58:34 +0300

    Complete_data_sending

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const https = require('https');
 const { resolve } = require('path');
 const xlsx = require('xlsx')
+const moment = require('moment');
+const { Cipher } = require('crypto');
 const fileDir = __dirname+"/stats.xlsx"
 
 let rawData = fs.readFileSync('auth-data.json')
@@ -22,16 +24,14 @@ const optionsPost = {
     method: 'POST',
   };
 
-const optionsGet = {
-    port: 443,
-    method: 'GET',
-};
   
 function authAndGetSessionID (login, pws){
     return new Promise(function(resolve, reject) {
         const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=auth&query=login&login=${login}&psw=${psw}`, optionsPost, (res) => {
             res.on('data', (data) => {
                 let sessionID = JSON.parse(data)['data'][0]['session']
+                let cookie = res.headers['set-cookie']
+                optionsPost.headers = {'Cookie': cookie}
                 if (sessionID == undefined) {
                     reject(new Error("No data"))
                 } else {
@@ -47,17 +47,15 @@ function authAndGetSessionID (login, pws){
     })
 }
 
-function getLsINFO(sessionID) {
+function getProviderInfo(sessionID) {
     return new Promise(function (resolve, reject) {
         const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=LSList&session=${sessionID}`, optionsPost, (res) => {
-            let cookie = (res.headers['set-cookie'][0]).slice(0,111)
             res.on('data', (data) => {
-                serviceID = JSON.parse(data)['data'][0]["id_service"]
-                if (serviceID == undefined) {
-                    reject(new Error("No serviceID"))
+                vl_provider = JSON.parse(data)['data'][0]["vl_provider"]
+                if (vl_provider == undefined) {
+                    reject(new Error("Can't find provider"))
                 } else {
-                    let arr = []
-                    arr.push(cookie, serviceID)
+                    let arr = [sessionID, vl_provider]
                     resolve (arr)
                 }
             })
@@ -68,38 +66,56 @@ function getLsINFO(sessionID) {
         req.end();
     }) 
 }
-authAndGetSessionID(login,psw).then( (sessionID)=> {
-    getLsINFO(sessionID).then((data) => {
-        let cookie = data[0]
-        let headers = {
-            'Cookie' : cookie
-        }
-        const req = https.request(`https://my.mosenergosbyt.ru/accounts/${serviceID}/tranfer-indications`, { port: 443, method: 'GET', headers:{}}, (res) => {
+
+function getCountersData(sessionID,vl_provider) {
+    return new Promise(function(resolve, reject) {
+        let counters = []
+        const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=smorodinaTransProxy&session=${sessionID}&plugin=smorodinaTransProxy&proxyquery=AbonentEquipment&vl_provider=%7B%22id_abonent%22%3A${vl_provider}%7D`, optionsPost, (res) => {
             res.on('data', (data) => {
-                console.log(JSON.parse(data))
-            })
+                JSON.parse(data)['data'].forEach(counterData => {
+                    let counter = {'id': counterData['id_counter'], 'name': counterData['nm_counter'], 'id_zn':counterData['id_counter_zn'], 'lastData': counterData['vl_last_indication'], 'descr': counterData['nm_service']}
+                    counters.push(counter)
+                });
+                if (counters.length === 0) {
+                    reject(new Error("No Counter Data"))
+                } else {
+                    resolve(counters)
+                }
+            } )
         })
+        req.on('error', (e) => {
+            console.error(e);
+        });
         req.end()
+    })
+}
+
+function sendIndications (sessionID, counters, vl_provider) {
+    return new Promise(function(resolve, reject) {
+        for(let idx = 0; idx < 1; idx++){
+            date = moment().format()
+            const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=AbonentSaveIndication&session=${sessionID}&dt_indication=${date}&id_counter=${counters[idx]['id']}&id_counter_zn=${counters[idx]['id_zn']}&id_source=15418&plugin=propagateMoeInd&pr_skip_anomaly=0&pr_skip_err=0&vl_indication=71&vl_provider=%7B%22id_abonent%22%3A${vl_provider}%7D`, 
+        optionsPost, (res) => {
+                res.on('data', (data) => {
+                    let nm_result = JSON.parse(data)['data']
+                    process.stdout.write(data)
+                })
+            })
+            req.end()
+        }
+    })
+}
+
+authAndGetSessionID(login,psw).then( sessionID => {
+    getProviderInfo(sessionID).then( data => {
+        let sessionID = data[0]
+        let vl_provider = data[1].slice(14,22)
+        getCountersData(sessionID, vl_provider).then(data => {
+            sendIndications(sessionID,data,vl_provider)
+        })
     })
 
 })
-
-
-
-
-// req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=GetProfileAttributesValues&session=${session}`, optionsPost, (res) => {
-//     console.log(JSON.parse(res))
-// })
-// req.on('error', (e) => {
-//   console.error(e);
-// });
-// req.end();
-    
-// https.get("https://my.mosenergosbyt.ru/accounts/10818835/transfer-indications", (res) => {
-//     console.log(res)
-//     console.log(__dirname+"/stats.xlsx")
-// })
-
 
 // function CollectDataFromExel(dir){
 //     const workbook = xlsx.readFile(dir)
