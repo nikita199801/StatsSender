8ccb72d43c489ba69648aa2ab8d7efe7d701e703
Nikita <nikita199801@gmail.com>
2021-01-18 15:40:43 +0300

    Save

--- app.js
+++ app.js
@@ -7,116 +7,120 @@ const moment = require('moment');
 const { Cipher } = require('crypto');
 const fileDir = __dirname+"/stats.xlsx"
 const {google} = require('googleapis')
+const counterStats = require('./gsheetconnection')
+
+let ls =counterStats()
+console.log(ls)
 
 let rawData = fs.readFileSync('auth-data.json')
 let authData = JSON.parse(rawData)
 let login = authData["login"]
 let psw  = authData["psw"]
 
-statsData = {
-    coldWater : 0,
-    hotWater: 0,
-    electro: 0,
-    heat: 0
-}
+// statsData = {
+//     coldWater : 0,
+//     hotWater: 0,
+//     electro: 0,
+//     heat: 0
+// }
 
-const optionsPost = {
-    port: 443, 
-    method: 'POST',
-  };
+// const optionsPost = {
+//     port: 443, 
+//     method: 'POST',
+//   };
 
   
-function authAndGetSessionID (login, pws){
-    return new Promise(function(resolve, reject) {
-        const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=auth&query=login&login=${login}&psw=${psw}`, optionsPost, (res) => {
-            res.on('data', (data) => {
-                let sessionID = JSON.parse(data)['data'][0]['session']
-                let cookie = res.headers['set-cookie']
-                optionsPost.headers = {'Cookie': cookie}
-                if (sessionID == undefined) {
-                    reject(new Error("No data"))
-                } else {
-                    resolve(sessionID)
-                }
-            })
-        })
-        req.on('error', (e) => {
-            console.error(e);
-        });
+// function authAndGetSessionID (login, pws){
+//     return new Promise(function(resolve, reject) {
+//         const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=auth&query=login&login=${login}&psw=${psw}`, optionsPost, (res) => {
+//             res.on('data', (data) => {
+//                 let sessionID = JSON.parse(data)['data'][0]['session']
+//                 let cookie = res.headers['set-cookie']
+//                 optionsPost.headers = {'Cookie': cookie}
+//                 if (sessionID == undefined) {
+//                     reject(new Error("No data"))
+//                 } else {
+//                     resolve(sessionID)
+//                 }
+//             })
+//         })
+//         req.on('error', (e) => {
+//             console.error(e);
+//         });
 
-        req.end()
-    })
-}
+//         req.end()
+//     })
+// }
 
-function getProviderInfo(sessionID) {
-    return new Promise(function (resolve, reject) {
-        const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=LSList&session=${sessionID}`, optionsPost, (res) => {
-            res.on('data', (data) => {
-                vl_provider = JSON.parse(data)['data'][0]["vl_provider"]
-                if (vl_provider == undefined) {
-                    reject(new Error("Can't find provider"))
-                } else {
-                    let arr = [sessionID, vl_provider]
-                    resolve (arr)
-                }
-            })
-        })
-        req.on('error', (e) => {
-            console.error(e);
-        });
-        req.end();
-    }) 
-}
+// function getProviderInfo(sessionID) {
+//     return new Promise(function (resolve, reject) {
+//         const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=LSList&session=${sessionID}`, optionsPost, (res) => {
+//             res.on('data', (data) => {
+//                 vl_provider = JSON.parse(data)['data'][0]["vl_provider"]
+//                 if (vl_provider == undefined) {
+//                     reject(new Error("Can't find provider"))
+//                 } else {
+//                     let arr = [sessionID, vl_provider]
+//                     resolve (arr)
+//                 }
+//             })
+//         })
+//         req.on('error', (e) => {
+//             console.error(e);
+//         });
+//         req.end();
+//     }) 
+// }
 
-function getCountersData(sessionID,vl_provider) {
-    return new Promise(function(resolve, reject) {
-        let counters = []
-        const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=smorodinaTransProxy&session=${sessionID}&plugin=smorodinaTransProxy&proxyquery=AbonentEquipment&vl_provider=%7B%22id_abonent%22%3A${vl_provider}%7D`, optionsPost, (res) => {
-            res.on('data', (data) => {
-                JSON.parse(data)['data'].forEach(counterData => {
-                    let counter = {'id': counterData['id_counter'], 'name': counterData['nm_counter'], 'id_zn':counterData['id_counter_zn'], 'lastData': counterData['vl_last_indication'], 'descr': counterData['nm_service']}
-                    counters.push(counter)
-                });
-                if (counters.length === 0) {
-                    reject(new Error("No Counter Data"))
-                } else {
-                    resolve(counters)
-                }
-            } )
-        })
-        req.on('error', (e) => {
-            console.error(e);
-        });
-        req.end()
-    })
-}
+// function getCountersData(sessionID,vl_provider) {
+//     return new Promise(function(resolve, reject) {
+//         let counters = []
+//         const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=smorodinaTransProxy&session=${sessionID}&plugin=smorodinaTransProxy&proxyquery=AbonentEquipment&vl_provider=%7B%22id_abonent%22%3A${vl_provider}%7D`, optionsPost, (res) => {
+//             res.on('data', (data) => {
+//                 JSON.parse(data)['data'].forEach(counterData => {
+//                     let counter = {'id': counterData['id_counter'], 'name': counterData['nm_counter'], 'id_zn':counterData['id_counter_zn'], 'lastData': counterData['vl_last_indication'], 'descr': counterData['nm_service']}
+//                     counters.push(counter)
+//                 });
+//                 if (counters.length === 0) {
+//                     reject(new Error("No Counter Data"))
+//                 } else {
+//                     resolve(counters)
+//                 }
+//             } )
+//         })
+//         req.on('error', (e) => {
+//             console.error(e);
+//         });
+//         req.end()
+//     })
+// }
 
-function sendIndications (sessionID, counters, vl_provider) {
-    return new Promise(function(resolve, reject) {
-        for(let idx = 0; idx < 1; idx++){
-            date = moment().format()
-            const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=AbonentSaveIndication&session=${sessionID}&dt_indication=${date}&id_counter=${counters[idx]['id']}&id_counter_zn=${counters[idx]['id_zn']}&id_source=15418&plugin=propagateMoeInd&pr_skip_anomaly=0&pr_skip_err=0&vl_indication=71&vl_provider=%7B%22id_abonent%22%3A${vl_provider}%7D`, 
-        optionsPost, (res) => {
-                res.on('data', (data) => {
-                    let nm_result = JSON.parse(data)['data']
-                    process.stdout.write(data)
-                })
-            })
-            req.end()
-        }
-    })
-}
+// function sendIndications (sessionID, counters, vl_provider) {
+//     return new Promise(function(resolve, reject) {
+//         for(let idx = 0; idx < 1; idx++){
+//             date = moment().format()
+//             const req = https.request(`https://my.mosenergosbyt.ru/gate_lkcomu?action=sql&query=AbonentSaveIndication&session=${sessionID}&dt_indication=${date}&id_counter=${counters[idx]['id']}&id_counter_zn=${counters[idx]['id_zn']}&id_source=15418&plugin=propagateMoeInd&pr_skip_anomaly=0&pr_skip_err=0&vl_indication=71&vl_provider=%7B%22id_abonent%22%3A${vl_provider}%7D`, 
+//         optionsPost, (res) => {
+//                 res.on('data', (data) => {
+//                     let nm_result = JSON.parse(data)['data']
+//                     process.stdout.write(data)
+//                 })
+//             })
+//             req.end()
+//         }
+//     })
+// }
 
-authAndGetSessionID(login,psw).then( sessionID => {
-    getProviderInfo(sessionID).then( data => {
-        let sessionID = data[0]
-        let vl_provider = data[1].slice(14,22)
-        getCountersData(sessionID, vl_provider).then(data => {
-            sendIndications(sessionID,data,vl_provider)
-        })
-    })
+// authAndGetSessionID(login,psw).then( sessionID => {
+//     getProviderInfo(sessionID).then( data => {
+//         let sessionID = data[0]
+//         let vl_provider = data[1].slice(14,22)
+//         getCountersData(sessionID, vl_provider).then(data => {
+//             sendIndications(sessionID,data,vl_provider)
+//         })
+//     })
 
-})
+// })
 
 // function CollectDataFromExel(dir){
 //     const workbook = xlsx.readFile(dir)
@@ -128,8 +132,8 @@ authAndGetSessionID(login,psw).then( sessionID => {
 //     console.log(JSON.stringify(statsData))
 // }
 
-https.createServer((req, res) => {
-    // CollectDataFromExel(fileDir)
-    res.write(req)
-}).listen(8080)
+// https.createServer((req, res) => {
+//     // CollectDataFromExel(fileDir)
+//     res.write(req)
+// }).listen(8080)
 
--- gsheetconnection.js
+++ gsheetconnection.js
@@ -1,21 +1,50 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const { fusiontables } = require('googleapis/build/src/apis/fusiontables');
+const { get } = require('http');
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 const TOKEN_PATH = 'token.json';
 
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), getCountersStats);
-  });
+let counterStats
 
-  
-  function authorize(credentials, callback) {
+function foo(params) {
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) console.log('Error loading client secret file:', err)
+    authorize(JSON.parse(content), getStats)
+    // Authorize a client with credentials, then call the Google Sheets API.
+  });
+}
+
+console.log(foo())
+
+function getStats(auth){
+  getCurrentRow(auth)
+  .then(currentRowVar => {
+    const currentRow = currentRowVar[0][0]
+    console.log (currentRow)
+    getCurrentStats(auth ,currentRow).then( stats => { 
+      counterStats = stats
+      console.log(counterStats)
+    })
+  }).then(()=>setRowVar(auth, currentRowVar))
+}
+
+// function getStats(auth) {
+//   getCurrentRow(auth)
+//   .then(currentRowVar => getCurrentStats(auth, currentRowVar[0][0]))
+//   .then(stats =>{
+//     counterStats = stats
+//     console.log(counterStats)
+//     // setRowVar(auth, currentRow)
+//   }).then(()=> setRowVar(auth, currentRow))
+// }
+
+
+
+function authorize(credentials, callback) {
     const {client_secret, client_id, redirect_uris} = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-  
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
       if (err) return getNewToken(oAuth2Client, callback);
@@ -24,47 +53,84 @@ fs.readFile('credentials.json', (err, content) => {
     });
   }
 
-  function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
 
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error while trying to retrieve access token', err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) return console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        callback(oAuth2Client);
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
       });
+      callback(oAuth2Client);
     });
-  }
+  });
+}
   
-  /**
-   * Prints the names and majors of students in a sample spreadsheet:
-   * @see https://docs.google.com/spreadsheets/d/1-RSIrL-tMbOUyBpy63WrQCMvo43OtUd-Nmn2pNOnH0M/edit
-   * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
-   */
-  function getCountersStats(auth) {
-    const sheets = google.sheets({version: 'v4', auth});
-    sheets.spreadsheets.values.get({
-      spreadsheetId: '1-RSIrL-tMbOUyBpy63WrQCMvo43OtUd-Nmn2pNOnH0M',
-      range: "'Лист1'!A2:E",
-      majorDimension:"ROWS"
-    }, (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const rows = res.data.values;
-      console.log(rows)
-    });
+  // /**
+  //  * Prints the names and majors of students in a sample spreadsheet:
+  //  * @see https://docs.google.com/spreadsheets/d/1-RSIrL-tMbOUyBpy63WrQCMvo43OtUd-Nmn2pNOnH0M/edit
+  //  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+  //  */
+
+  function getCurrentRow(auth){
+    return new Promise(function (resolve, reject){
+      const sheets = google.sheets({version: 'v4', auth});
+      sheets.spreadsheets.values.get({
+        spreadsheetId: '1-RSIrL-tMbOUyBpy63WrQCMvo43OtUd-Nmn2pNOnH0M',
+        range: "'Лист1'!J2",
+      }, (err, res) => {
+        if (err) reject('The API returned an error: ' + err)
+        resolve(res.data.values)
+      })
+    })
   }
+
+  function getCurrentStats(auth, row){
+    return new Promise(function(resolve, reject){
+      const sheets = google.sheets({version: 'v4', auth});
+      sheets.spreadsheets.values.get({
+        spreadsheetId: '1-RSIrL-tMbOUyBpy63WrQCMvo43OtUd-Nmn2pNOnH0M',
+        range: "'Лист1'!A2:E",
+      }, (err, res) => {
+        if (err) reject('The API returned an error: ' + err)
+        const rows = res.data.values;
+        (rows[row][4])?resolve(rows[row]):reject("This stats already sent")
+      });
+    })
+  }
+
+ async function setRowVar(auth, currentRowVar) {
+  const sheets = google.sheets({version: 'v4', auth});
+    let values = [[Number(currentRowVar)+1]]
+    const resource = {
+      values
+    }
+    sheets.spreadsheets.values.update({
+      spreadsheetId: '1-RSIrL-tMbOUyBpy63WrQCMvo43OtUd-Nmn2pNOnH0M',
+      range: "J2",
+      valueInputOption:'RAW',
+      resource: resource
+    }, (err, result) =>{
+      if(err){
+        console.log(err)
+      } else {
+        console.log("Updated J2 %d", values[0][0])
+      }
+    })
+  }
+
+  // module.exports = getStatsFromSpreadsheet
