0ac4666ebb900048d6980678b26261cbb4e6b26b
Nikita <nikita199801@gmail.com>
2021-01-22 14:54:21 +0300

    no msg

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const runServer = require('./server')
 
-console.log(process.env.TOKEN)
+console.log(process.env)
 
 fs.appendFileSync('./token.json', `${process.env.TOKEN}`, {format: 'a+'})
 fs.appendFileSync('./credentials.json', `${process.env.CREDENTIALS}`, {format: 'a+'})
