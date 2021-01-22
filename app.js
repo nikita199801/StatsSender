a5b11f2bfc02ffbfb2b85db4166e82fd180593fe
Nikita <nikita199801@gmail.com>
2021-01-22 15:39:36 +0300

    no msg

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const runServer = require('./server')
 
 console.log(process.env)
 
-fs.writeFileSync('./token.json', `${process.env.TOKEN}`, {format: 'a+'})
-fs.writeFileSync('./credentials.json', `${process.env.CREDENTIALS}`, {format: 'a+'})
-fs.writeFileSync('./auth-data.json', `${process.env.AUTH_DATA}`, {format: 'a+'})
+fs.writeFileSync('./token.json', `${JSON.stringify(process.env.TOKEN)}`, {format: 'a+'})
+fs.writeFileSync('./credentials.json', `${JSON.stringify(process.env.CREDENTIALS)}`, {format: 'a+'})
+fs.writeFileSync('./auth-data.json', `${JSON.stringify(process.env.AUTH_DATA)}`, {format: 'a+'})
 runServer()
