cdf3d2efea06a0d8291ff35d6052977c33f25480
Nikita <nikita199801@gmail.com>
2021-01-22 13:54:44 +0300

    commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const runServer = require('./server')
 
 fs.appendFileSync('./token.json', `${process.env.TOKEN}`, {format: 'a+'})
-fs.appendFileSync('./token.json', `${process.env.CREDENTIALS}`, {format: 'a+'})
-fs.appendFileSync('./token.json', `${process.env.AUTH_DATA}`, {format: 'a+'})
+fs.appendFileSync('./credentials.json', `${process.env.CREDENTIALS}`, {format: 'a+'})
+fs.appendFileSync('./auth-data.json', `${process.env.AUTH_DATA}`, {format: 'a+'})
 runServer()
